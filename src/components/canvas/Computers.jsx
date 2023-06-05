13a331fdeb9b301a9f5f4276f2955abfbfe25154
prashant-kr5117 <prashantkumar5395@gmail.com>
2023-06-06 01:50:50 +0530

    Wrapper and about section added

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-unknown-property */
-// import {Suspense,useEffect,useState} from 'react'
-import { Suspense } from "react";
+import { Suspense, useState, useEffect } from "react";
 
 // canva is just a empty canvas on which allow us to place something on it
 import { Canvas } from "@react-three/fiber";
@@ -11,7 +10,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   // importing our GLTF Model
 
   const computer = useGLTF("../../../public/desktop_pc/scene.gltf");
@@ -30,8 +29,8 @@ const Computers = () => {
 
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.55, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.55, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -39,6 +38,28 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    //Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width:500px)");
+
+    //set the initial value of the 'isMobile' state variable
+    setIsMobile(mediaQuery.matches);
+
+    //Define a call back function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+//Add the call back function as a listener for changes to the media query 
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the callback function as a listener for changes to the media query
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
   return (
     <Canvas
       frameloop="demand"
@@ -54,7 +75,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
